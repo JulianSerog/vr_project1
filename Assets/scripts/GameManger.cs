30bd809a4b90c9089bf89c966aaba719389a6a5f
Esa Julian Serog <esa5959@yahoo.com>
2017-03-12 17:46:02 -0400

    first commit

---
+++ Assets/scripts/GameManger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour {
+
+	public MainMenuController mainMenu;
+	public Camera initialCamera;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void startScenario1 () {
+		//set menu and current camera to false
+		mainMenu.gameObject.SetActive(false);
+		initialCamera.gameObject.SetActive(false);
+
+		//load scene layout
+
+	}
+
+	public void startScenario2() {
+		//set menu and current camera to false
+		mainMenu.gameObject.SetActive(false);
+		initialCamera.gameObject.SetActive(false);
+
+		//load scene layout
+	}
+
+	public void startDiamondBreak() {
+		//set menu and current camera to false
+		mainMenu.gameObject.SetActive(false);
+		initialCamera.gameObject.SetActive(false);
+
+
+		//load diamond break layout
+	}
+
+	public void startTriangleBreak() {
+		//set menu and current camera to false
+		mainMenu.gameObject.SetActive(false);
+		initialCamera.gameObject.SetActive(false);
+
+		//load Triangle Break Layout
+	}
+
+	public void backToMainMenu() {
+		//set other items to inactive
+
+
+		//set main menu and initial camera active
+		mainMenu.gameObject.SetActive(true);
+		initialCamera.gameObject.SetActive(true);
+	}
+}
---
+++ Assets/scripts/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	public GameManger gameManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void startScenario1Clicked () {
+		gameManager.startScenario1();
+		Debug.Log ("Scenario 1");
+	}
+
+	public void startScenario2Clicked() {
+		gameManager.startScenario2();
+		Debug.Log ("Scenario 2");
+	}
+
+	public void startDiamondBreakClicked() {
+		gameManager.startDiamondBreak ();
+		Debug.Log ("Diamond Break");
+	}
+
+	public void startTriangleBreakClicked() {
+		gameManager.startTriangleBreak ();
+		Debug.Log ("triangle break");
+	}
+}
