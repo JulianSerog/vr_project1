c342cc193ce41e3cbf0c0dc885d4dabb656f14bc
kjjohnsen <kjohnsen@uga.edu>
2017-03-16 14:24:49 -0400

    added laser to esa branch and made laser smaller.

---
+++ Assets/scripts/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+	LineRenderer line;
+	// Use this for initialization
+	void Start () {
+		line = gameObject.GetComponent<LineRenderer> ();
+		//line.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Ray laser = new Ray (transform.position, transform.up);
+		RaycastHit laserHit;
+		line.SetPosition (0, laser.origin);
+
+		if (Physics.Raycast (laser, out laserHit, 200)) {
+			line.SetPosition (1, laserHit.point);	
+		} else {
+			line.SetPosition (1, laser.GetPoint(200));
+
+		}
+	}
+}
