73e0c6ba20f52b6b84f094a6c988d5da3cf91300
kjjohnsen <kjohnsen@uga.edu>
2017-03-17 19:22:38 -0400

    added highlights for cubes that are selected

--- Assets/scripts/Laser.cs
+++ Assets/scripts/Laser.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour {
 
+	//ray cast
 	LineRenderer line;
-
 	public RaycastHit laserHit;
 	public GameManger manager;
+	//cubes
+	public GameObject triangleBreakCube;
+	public GameObject diamondBreakCube;
+	public GameObject scenario1Cube;
+	public GameObject scenario2Cube;
+	//materials
+	public Material selected;
+	public Material defaultMat;
+	//cube array for shorter code
+	GameObject[] cubes;
 
 
 	// Use this for initialization
 	void Start () {
 		line = gameObject.GetComponent<LineRenderer> ();
 		//line.enabled = false;
+		cubes = new GameObject[] {triangleBreakCube, diamondBreakCube, scenario1Cube, scenario2Cube};
 	}
 
 
@@ -26,22 +37,49 @@ public class Laser : MonoBehaviour {
 			line.SetPosition (1, laserHit.point);	
 		} else {
 			line.SetPosition (1, laser.GetPoint(200));
-
+			//change cubes to default material if nothing is selected
+			for(int i = 0; i < cubes.Length; i++) {
+				cubes[i].GetComponent<Renderer>().material = defaultMat;
+			}
 		}//else
 
 
 		//TODO: get button press in addition to collision to trigger actions
 		if (laserHit.collider.name == "TriangleBreak Cube") {
+			
+			for(int i = 0; i < cubes.Length; i++) {
+				if(i == 0) cubes[i].GetComponent<Renderer>().material = selected;
+				else cubes[i].GetComponent<Renderer>().material = defaultMat;
+			}
+			//set up scene
 			manager.startTriangleBreak ();
 		} else if (laserHit.collider.name == "Diamond Break Cube") {
+			for(int i = 0; i < cubes.Length; i++) {
+				if(i == 1) cubes[i].GetComponent<Renderer>().material = selected;
+				else cubes[i].GetComponent<Renderer>().material = defaultMat;
+			}
+			//set up scene
 			manager.startDiamondBreak ();
 		} else if (laserHit.collider.name == "Scenario1 Cube") {
+			for(int i = 0; i < cubes.Length; i++) {
+				if(i == 2) cubes[i].GetComponent<Renderer>().material = selected;
+				else cubes[i].GetComponent<Renderer>().material = defaultMat;
+			}
+			//set up scene
 			manager.startScenario1 ();
 		} else if (laserHit.collider.name == "Scenario2 Cube") {
+			for(int i = 0; i < cubes.Length; i++) {
+				if(i == 3) cubes[i].GetComponent<Renderer>().material = selected;
+				else cubes[i].GetComponent<Renderer>().material = defaultMat;
+			}
+			//set up scene
 			manager.startScenario2 ();
-		}//else ifs
-
-	
-	
-	}
-}
+		} else {
+			//remove material from all cubes
+			for(int i = 0; i < cubes.Length; i++) {
+				cubes[i].GetComponent<Renderer>().material = defaultMat;
+			}//for
+			
+		}//else
+	}//Update
+}//Class
